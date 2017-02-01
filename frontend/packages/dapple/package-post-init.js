d9abb28f9cc3e070f30b53c9211b6aee56016ae7
Mariano Conti <nanexcool@gmail.com>
2017-02-01 17:27:18 -0300

    Add VSL token

--- frontend/packages/dapple/package-post-init.js
+++ frontend/packages/dapple/package-post-init.js
@@ -53,6 +53,7 @@ const tokens = {
     REP: '0xf75caa57375a75dfc1a7ea917d6abb2c95511053',
     ICN: '0x5b73d26807ea72287bafa1a27fccf8ece5deabc4',
     '1ST': '0xa8c784efdfe7d48bc5df28f770b6454a037e2abe',
+    VSL: '0x5017f42cf680fcbcab1093263468745c9af63e35',
   },
   morden: {
     'W-ETH': '0x52fe88b987c7829e5d5a61c98f67c9c14e6a7a90',
@@ -64,6 +65,7 @@ const tokens = {
     REP: '0x0000000000000000000000000000000000000000',
     ICN: '0x0000000000000000000000000000000000000000',
     '1ST': '0x0000000000000000000000000000000000000000',
+    VSL: '0x0000000000000000000000000000000000000000',
   },
   live: {
     'W-ETH': '0xecf8f87f810ecf450940c9f60066b4a7a501d6a7',
@@ -75,6 +77,7 @@ const tokens = {
     REP: '0x48c80f1f4d53d5951e5d5438b54cba84f29f32a5',
     ICN: '0x888666ca69e0f178ded6d75b5726cee99a87d698',
     '1ST': '0xaf30d2a7e90d7dc361c8c4585e9bb7d2f6f15bc7',
+    VSL: '0x5c543e7ae0a1104f78406c340e9c64fd9fce5170',
   },
 };
 
@@ -89,13 +92,14 @@ const tokenSpecs = {
   REP: { precision: 18, format: '0,0.00[0000000000000000]' },
   ICN: { precision: 18, format: '0,0.00[0000000000000000]' },
   '1ST': { precision: 18, format: '0,0.00[0000000000000000]' },
+  VSL: { precision: 18, format: '0,0.00[0000000000000000]' },
 };
 
 Dapple.getQuoteTokens = () => ['W-ETH'];
 
-Dapple.getBaseTokens = () => ['MKR', /* 'DAI', */'DGD', 'W-GNT', 'REP', 'ICN', '1ST'];
+Dapple.getBaseTokens = () => ['MKR', /* 'DAI', */'DGD', 'W-GNT', 'REP', 'ICN', '1ST', 'VSL'];
 
-Dapple.getTokens = () => ['W-ETH', 'MKR', /* 'DAI', */'DGD', 'GNT', 'W-GNT', 'REP', 'ICN', '1ST'];
+Dapple.getTokens = () => ['W-ETH', 'MKR', /* 'DAI', */'DGD', 'GNT', 'W-GNT', 'REP', 'ICN', '1ST', 'VSL'];
 
 Dapple.getTokenSpecs = (symbol) => {
   if (typeof (tokenSpecs[symbol]) !== 'undefined') {
